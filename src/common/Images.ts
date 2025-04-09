3b069736665ffbbff71f2e12a2c33fafc13f050b
HyeRrin <candy980426@gmail.com>
2025-04-09 23:16:58 +0900

    [add] Logo Image Assets

---
+++ src/common/Images.ts
@@ -0,0 +1,7 @@
+import LogoIcon from "../images/logo-icon.png";
+import LogoTitle from "../images/logo-title.png";
+
+export default {
+  LogoIcon,
+  LogoTitle,
+};
--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export { default as Colors } from "./Colors";
 export { default as Routes } from "./Routes";
 export { default as Config } from "./Config";
 export { default as Constants } from "./Constants";
+export { default as Images } from "./Images";
