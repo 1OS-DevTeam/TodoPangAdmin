d817780c387ee51c2fa1558182f2b75e252ecb9b
HyeRrin <candy980426@gmail.com>
2025-04-09 23:20:13 +0900

    [modify] API Base URL

--- src/common/Config.ts
+++ src/common/Config.ts
@@ -1,4 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
-const SERVER_URL = "https://todopang.uk/admin";
+
+const DEV_URL = "https://todopang.uk/admin";
+const PROD_URL = "http://localhost:18540/admin";
+
+const SERVER_URL = isProd ? PROD_URL : DEV_URL;
 
 export default { SERVER_URL, isProd };
