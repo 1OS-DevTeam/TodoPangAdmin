58fae1ddf8f99d02dc94ed087319402f59e0a546
HyeRrin <candy980426@gmail.com>
2025-04-20 23:12:39 +0900

    [fix] SERVER_URL

--- src/common/Config.ts
+++ src/common/Config.ts
@@ -1,8 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
-const DEV_URL = "https://todopang.uk/admin";
-const PROD_URL = "http://localhost:18540/admin";
-
-const SERVER_URL = isProd ? PROD_URL : DEV_URL;
+const SERVER_URL = "https://todopang.uk/admin";
 
 export default { SERVER_URL, isProd };
